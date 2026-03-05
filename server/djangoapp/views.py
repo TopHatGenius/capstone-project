ac2aaed8915acc62a2f96708c5b4274f4276e43e
TopHatGenius <bezerker87@gmail.com>
2026-03-05 12:15:43 -0800

    Up to task 5

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -12,7 +12,7 @@ import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
 from .populate import initiate
-from .restapis import get_request, analyze_review_sentiments
+from .restapis import get_request, analyze_review_sentiments, post_review
 
 from .models import CarMake, CarModel
 
@@ -105,7 +105,10 @@ def get_dealer_reviews(request, dealer_id):
         for review_detail in reviews:
             response = analyze_review_sentiments(review_detail['review'])
             print(response)
-            review_detail['sentiment'] = response['sentiment']
+            if response is not None:
+                review_detail['sentiment'] = response['sentiment']
+        else:
+            review_detail['sentiment'] = 'neutral'
         return JsonResponse({"status":200,"reviews":reviews})
     else:
         return JsonResponse({"status":400,"message":"Bad Request"})
