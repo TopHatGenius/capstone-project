495c45788c2a613407a4ad90c7407a05b0fe98a9
TopHatGenius <bezerker87@gmail.com>
2026-03-12 13:45:48 -0700

    More refactoring. Added config file to tell JSHint to use ES6

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -38,11 +38,13 @@ def login_user(request):
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
 
+
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
     logout(request)
     data = {"userName": ""}
     return JsonResponse(data)
+
 
 # Create a `registration` view to handle sign up request
 @csrf_exempt
@@ -69,7 +71,10 @@ def registration(request):
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,password=password, email=email)
+        user = User.objects.create_user(username=username,
+                                        first_name=first_name,
+                                        last_name=last_name,
+                                        password=password, email=email)
         # Login the user and redirect to list page
         login(request, user)
         data = {"userName": username,"status": "Authenticated"}
@@ -77,6 +82,7 @@ def registration(request):
     else:
         data = {"userName": username,"error": "Already Registered"}
         return JsonResponse(data)
+
 
 def get_cars(request):
     count = CarMake.objects.filter().count()
@@ -86,14 +92,14 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({"CarModel": car_model.name,
+                     "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
 # def get_dealerships(request):
 # ...
-
 
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
@@ -113,8 +119,9 @@ def get_dealer_reviews(request, dealer_id):
     else:
         return JsonResponse({"status": 400,"message": "Bad Request"})
 
+
 # Create a `get_dealer_details` view to render the dealer details
-def get_dealer_details(request, dealer_id): 
+def get_dealer_details(request, dealer_id):
     if (dealer_id): 
         endpoint = "/fetchDealer/"+str(dealer_id)
         dealership = get_request(endpoint)
@@ -122,19 +129,23 @@ def get_dealer_details(request, dealer_id):
     else:
         return JsonResponse({"status": 400,"message":"Bad Request"})
 
+
 # Create a `add_review` view to submit a review
-def add_review(request): 
+def add_review(request):
     if not request.user.is_anonymous:
         data = json.loads(request.body)
         try: 
-            response = post_review(data)
+            post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
-            return JsonResponse({"status": 401,"message": "Error in posting review"})
+            return JsonResponse({"status": 401,
+                                 "message": "Error in posting review"})
     else: 
         return JsonResponse({"status": 403,"message":"Unauthorized"})
 
-#Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
+
+# Update the `get_dealerships` render list of dealerships all by default,
+# particular state if state is passed
 def get_dealerships(request, state="All"):
     if (state == "All"):
         endpoint = "/fetchDealers"
